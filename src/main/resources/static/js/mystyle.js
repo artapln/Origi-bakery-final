194f2c2eca1d1a6fa5d1b9964271cb03804c73b4
artapln <50373007+artapln@users.noreply.github.com>
2020-12-02 22:07:20 +0700

    Add files via upload

---
+++ src/main/resources/static/js/mystyle.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.navbar-xbootstrap').click(function(){
+      $('.nav-xbootstrap').toggleClass('visible');
+      $('body').toggleClass('cover-bg');
+    });
+  });
+
+
+
